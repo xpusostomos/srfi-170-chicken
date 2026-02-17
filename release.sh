bdfa4bc0e72e1fa9264076e591846e3b1385c8ee
Chris Bitmead <xpusostomos@gmail.com>
2026-02-17 13:18:58 +0800

    add release.sh

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+VERSION=$1
+if [ -z "$VERSION" ]; then
+  echo "Usage: $0 version"
+  exit 1
+fi
+
+TAG="v$VERSION"
+
+# 1. Update .egg file version
+sed -i "s/(version \".*\")/(version \"$VERSION\")/" srfi-170.egg
+
+# 2. Add version to .release-info (before the last closing parenthesis)
+# This assumes a specific format; it's often easier to just edit manually
+# but here is a basic append logic:
+sed -i "/(tags/a \  (\"$VERSION\" \"$TAG\")" srfi-170.release-info
+
+# 3. Git dance
+git add srfi-170.egg srfi-170.release-info
+git commit -m "Release $VERSION"
+git tag -a $TAG -m "Version $VERSION"
+git push origin master --tags
+
+echo "Release $VERSION pushed to GitHub!"
